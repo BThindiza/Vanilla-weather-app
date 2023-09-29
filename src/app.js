ca2b84d24332535e482353e1133d2df82d80a563
Boitumelo Thindiza <tthindiza@gmail.com>
2023-09-29 06:24:49 -0700

    wind and humidity array

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@
 function displayTemperature(response){
     let temperatureelement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
-    let descriptionElement =Document.querySelector("#description");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
     temperatureelement.innerHTML= Math.round(response.data.main.temp);
 cityElement.innerHTML= response.data.name;
-descriptionElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML= response.data.main.humidity;
+windElement.innerHTML = math.round(response.data.wind.speed);
 }
+
 let apiKey ="3t1a5685d95o5fd95bdaaac3a43d5083";
 let apiUrl="https://api.shecodes.io/weather/v1/temperature?lon=-9.13333&lat=38.7166&key=3t1a5685d95o5fd95bdaaac3a43d5083&units=metric"; 
 axios.get(apiUrl).then(displayTemperature);
