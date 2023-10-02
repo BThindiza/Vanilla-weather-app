8debe6841943884ff71e709b19d4f22b4b2664d3
Boitumelo Thindiza <tthindiza@gmail.com>
2023-10-02 04:03:45 -0700

    Display day and time

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 function formatDate(timestamp){
-    
+let date= new Date(timestamp);
+let hours= date.getHours();
+if (hours < 10){
+    hours =`0${hours}`;
+} 
+let minutes= date.getMinutes();
+if (minutes< 10){
+    minutes =`0${hours}`;
+} 
+let days=["Sunday", "Monday", "Tuesday", "Wednesday" ,"Thursday","Friday","Saturday"];
+let months =["January","February","March","April","May","June","July","August","September","October","November","December"];
+let month = months[date.getMonth()];
+let day =  days[date.getDay()];
+return `${day} ${month}, ${hours}:${minutes}`;
 }
+
 
 function displayTemperature(response){
     let temperatureelement = document.querySelector("#temperature");
@@ -8,13 +22,13 @@ function displayTemperature(response){
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
-    let dateElelment.innerHTML("#date");
+    let dateElelment = document.querySelector("#date");
     temperatureelement.innerHTML= Math.round(response.data.main.temp);
 cityElement.innerHTML= response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML= response.data.main.humidity;
 windElement.innerHTML = math.round(response.data.wind.speed);
-dateElelment.innerHTML="Friday. 12;00";
+dateElelment.innerHTML= formatDate(response.data.dt*1000);
 }
 
 let apiKey ="3t1a5685d95o5fd95bdaaac3a43d5083";
