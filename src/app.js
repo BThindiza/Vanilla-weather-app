95760f11775db02c36d082356d4e9f610e9677cd
Boitumelo Thindiza <tthindiza@gmail.com>
2023-10-11 12:34:48 -0700

    Unit Conversion celsius and fahrenheit

--- src/app.js
+++ src/app.js
@@ -30,12 +30,12 @@ humidityElement.innerHTML= response.data.main.humidity;
 windElement.innerHTML = math.round(response.data.wind.speed);
 dateElelment.innerHTML= formatDate(response.data.dt*1000);
 }
+celsiusTemperature = response.data.main.temp;
 function search(city){
     let apiKey ="3t1a5685d95o5fd95bdaaac3a43d5083";
 let city ="Polokwane";
 let apiUrl="https://api.shecodes.io/weather/v1/current?lon=-9.13333&lat=38.7166&key=3t1a5685d95o5fd95bdaaac3a43d5083&units=metric"; 
 axios.get(apiUrl).then(displayTemperature);
-
 }
 
 function handleSubmit(event){
@@ -43,7 +43,27 @@ function handleSubmit(event){
     let citySearchElement = document.querySelector("#city-search");
     search(citySearchElement.value);
 }
-search("Polokwane");
+ function displayFahrenheitTemperature(event){
+    event.preventDefault();
+    let FahrenheitTemperature =(celsiusTemperature *9 ) /5 * 32;
+    let temperatureElement= document.querySelector("#temperature");
+    temperatureElement.innerHTML =Math.round(FahrenheitTemperature);
+ }
+
+ function  displayCelsiusTemperature(event){
+event.preventDefault();
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = Math.round(celsiusTemperature);
+ }
+let celsiusTemperature =null;
 
 let form = document.querySelector("#form");
 form.addEventListener("submit",handleSubmit);
+
+let fahrenheitLink= document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink= document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Polokwane");
