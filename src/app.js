023a99e8bec1782713222d5df251bc2ec4a46329
Boitumelo Thindiza <tthindiza@gmail.com>
2023-10-16 09:14:40 -0700

    fixed my functions

--- src/app.js
+++ src/app.js
@@ -9,12 +9,46 @@ if (minutes< 10){
     minutes =`0${hours}`;
 } 
 let days=["Sunday", "Monday", "Tuesday", "Wednesday" ,"Thursday","Friday","Saturday"];
-let months =["January","February","March","April","May","June","July","August","September","October","November","December"];
-let month = months[date.getMonth()];
 let day =  days[date.getDay()];
 return `${day} ${month}, ${hours}:${minutes}`;
 }
+function formatDay(timestamp){
+    let date =new Date(timestamp * 1000);\
+    let day =date.getDay();
+    let days =["Sun", "Mon","Tues","Wed","Thurs","Fri","Sat"];
+    return days[day];
+}
+function displayForescast(response){
+    let forecast = response.data.daily;
+let forecastElement = document.querySelector("#weather-forecast");
+let forecastHTML =`<div class = "row">`;
+forecast.forEach(function(forecast,index){
+    if (index < 6){
+        forecastHTML = forecastHTML + ` < div class ="col-2">
+        <div class ="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img src = "http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperature">
+        <span class = "weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)} °</span>
+        <span class ="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}° </span>
+        </div>
+        </div>
+        `;
+}
+});
 
+forecastHTML = forecastHTML + `</div>`;
+forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates){
+    let apiKey = "6ee72f51667c1ac4a6bc6bfa1cc12d42";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${ApiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForescast);
+}
 
 function displayTemperature(response){
     let temperatureelement = document.querySelector("#temperature");
@@ -23,6 +57,8 @@ function displayTemperature(response){
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElelment = document.querySelector("#date");
+    let iconElement = document.querySelector(icon);
+    
     temperatureelement.innerHTML= Math.round(response.data.main.temp);
 cityElement.innerHTML= response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
@@ -32,7 +68,7 @@ dateElelment.innerHTML= formatDate(response.data.dt*1000);
 }
 celsiusTemperature = response.data.main.temp;
 function search(city){
-    let apiKey ="3t1a5685d95o5fd95bdaaac3a43d5083";
+    let apiKey ="6ee72f51667c1ac4a6bc6bfa1cc12d42";
 let city ="Polokwane";
 let apiUrl="https://api.shecodes.io/weather/v1/current?lon=-9.13333&lat=38.7166&key=3t1a5685d95o5fd95bdaaac3a43d5083&units=metric"; 
 axios.get(apiUrl).then(displayTemperature);
