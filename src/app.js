fd6a54173b478d4e5af6bd01d9d085e6c38952da
Boitumelo Thindiza <tthindiza@gmail.com>
2023-10-17 11:56:42 -0700

    improved my search button

--- src/app.js
+++ src/app.js
@@ -57,20 +57,25 @@ function displayTemperature(response){
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElelment = document.querySelector("#date");
-    let iconElement = document.querySelector(icon);
-    
+    let iconElement = document.querySelector("#icon");
+
+     let celsiusTemperature = response.data.main.temp;
+
     temperatureelement.innerHTML= Math.round(response.data.main.temp);
 cityElement.innerHTML= response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML= response.data.main.humidity;
 windElement.innerHTML = math.round(response.data.wind.speed);
 dateElelment.innerHTML= formatDate(response.data.dt*1000);
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+);
+iconElement.setAttribute("alt",response.data.weather[0].description);
+getForecast(response.data.coord);
 }
-celsiusTemperature = response.data.main.temp;
+
 function search(city){
     let apiKey ="6ee72f51667c1ac4a6bc6bfa1cc12d42";
-let city ="Polokwane";
-let apiUrl="https://api.shecodes.io/weather/v1/current?lon=-9.13333&lat=38.7166&key=3t1a5685d95o5fd95bdaaac3a43d5083&units=metric"; 
+let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
 }
 
