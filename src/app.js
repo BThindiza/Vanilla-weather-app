b17fee45cd41d9a934836750fa14b3e150fc36a8
Boitumelo Thindiza <tthindiza@gmail.com>
2023-10-30 09:47:14 -0700

    Used a new apikey

--- src/app.js
+++ src/app.js
@@ -12,56 +12,6 @@ let days=["Sunday", "Monday", "Tuesday", "Wednesday" ,"Thursday","Friday","Satur
 let day =  days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 }
-function formatDay(timestamp){
-    let date = new Date(timestamp * 1000);
-    let day = date.getDay();
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday" ,"Thursday","Friday","Saturday"];
-
-    return days[day];
-}
-
-function displayForecast(response) {
-    let forecast = response.data.daily;
-  
-    let forecastElement = document.querySelector("#forecast");
-  
-    let forecastHTML = `<div class="row">`;
-    forecast.forEach(function (forecastDay, index) {
-      if (index < 6) {
-        forecastHTML =
-          forecastHTML +
-          `
-        <div class="col-2">
-          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-          <img
-            src="http://openweathermap.org/img/wn/${
-              forecastDay.weather[0].icon
-            }@2x.png"
-            alt=""
-            width="42"
-          />
-          <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max"> ${Math.round(
-              forecastDay.temp.max
-            )}° </span>
-            <span class="weather-forecast-temperature-min"> ${Math.round(
-              forecastDay.temp.min
-            )}° </span>
-          </div>
-        </div>
-    `;
-      }
-    });
-    forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
-}
-
-function getForecast(coordinates) {
-  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
-}
-
 
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
@@ -75,12 +25,12 @@ function displayTemperature(response) {
     let celsiusTemperature =response.data.main.temp;
     
         temperatureElement.innerHTML= Math.round(celsiusTemperature);
-cityElement.innerHTML= response.data.city;
+cityElement.innerHTML= response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML= response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
 dateElelment.innerHTML= formatDate(response.data.time*1000);
-iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@01d.png`);
 iconElement.setAttribute("alt",response.data.weather[0].description);
 
 getForecast(response.data.coord);
@@ -94,8 +44,8 @@ axios.get(apiUrl).then(displayTemperature);
 }
 function handleSubmit(event) {
     event.preventDefault();
-    let citySearchElement = document.querySelector("#city-search");
-    search(citySearchElement.value);
+    let cityElement = document.querySelector("#city");
+    search(cityElement.value);
 }
  function displayFahrenheitTemperature(event){
     event.preventDefault();
@@ -113,15 +63,15 @@ function handleSubmit(event) {
     temperatureElement.innerHTML = Math.round(celsiusTemperature);
  }
 
-let celsiusTemperature= null;
 
 let form =document.querySelector("#form");
  form.addEventListener("submit",handleSubmit);
+ 
+ search("Polokwane");
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click",displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
+celsiusLink.addEventListener("click",displayCelsiusTemperature);
 
-search("Polokwane");
